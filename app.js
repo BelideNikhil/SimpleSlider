5f80363eab10a9dc02af96ec4397b52ac559cdde
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-09-15 01:55:48 +0530

    clean-code

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-console.warn("I'm running")
 const inputs=document.querySelectorAll("input[name]");
-
-function handler(){
-    console.log(this)
-    // or '' is so that if no data-attribute then it is ''
-    // here we are basically taking the data-size attribute value and passing it to suffix so that we can append it to inpue value
-    const suffix=this.dataset.size || ''
-    document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix)
-}
 
 inputs.forEach(input => {
     input.addEventListener("mousemove",handler);
     input.addEventListener("change",handler);
-});
+})
+
+function handler(e){
+    let targetInput=e.target
+    const suffix=targetInput.dataset.size
+    document.body.style.setProperty(`--${targetInput.name}`,targetInput.value+suffix)
+}
+
